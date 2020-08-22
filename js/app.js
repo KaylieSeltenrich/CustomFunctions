cb988da174dbade5a82bb638d32f58047afec920
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-08-22 16:22:31 -0600

    Tested each function three times

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
+// Function 1
+
 function addAndMultiply(numberOne, numberTwo) {
     var addNumbersMultiplyFive = (numberOne + numberTwo) * 5;
     return addNumbersMultiplyFive;
 }
+
+// Function 2 
 
 function stringLengthIdentifer(string) {
 
@@ -15,6 +19,8 @@ function stringLengthIdentifer(string) {
 
 }
 
+// Function 3
+
 function phStringIdentifer(stringArray) {
     for (var i = 0; i < stringArray.length; i++) {
 
@@ -27,14 +33,32 @@ function phStringIdentifer(stringArray) {
     }
 }
 
-
+// Test Function 1 
 var result = addAndMultiply(5, 5);
 console.log(result);
+var result = addAndMultiply(10, 5);
+console.log(result);
+var result = addAndMultiply(5, 2);
+console.log(result);
 
-
+// Test Function 2
 var lengthAnswer = stringLengthIdentifer("String of Text");
 console.log(lengthAnswer);
+var lengthAnswer = stringLengthIdentifer("String");
+console.log(lengthAnswer);
+var lengthAnswer = stringLengthIdentifer("A longer string of Text");
+console.log(lengthAnswer);
+
+//Test Function 3
 
 var stringInput = ["book", "lamp", "phone", "school", "philosophy"];
+var stringResult = phStringIdentifer(stringInput);
+console.log(stringResult);
+
+var stringInput = ["book", "phil", "lamp", "school", "philosophy"];
+var stringResult = phStringIdentifer(stringInput);
+console.log(stringResult);
+
+var stringInput = ["phony", "lamp", "cake", "school", "potato"];
 var stringResult = phStringIdentifer(stringInput);
 console.log(stringResult);
